3499942824e8a568fe8d6cc888dff525a83acec3
parkjusang <jusang26@naver.com>
2021-05-17 14:11:17 +0900

    upload files

---
+++ busan (1)/src/main/java/com/itbank/service/TourService.java
@@ -0,0 +1,30 @@
+package com.itbank.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.itbank.tour.TourDAO;
+import com.itbank.tour.TourDTO;
+
+@Service
+public class TourService {
+   
+   @Autowired private TourDAO dao;
+   
+   public List<TourDTO> getTourList() {
+      List<TourDTO> list = dao.getTourList();
+      return list;
+   }
+
+   public TourDTO getSelectOne(String tname) {
+      TourDTO dto = dao.getSelectOne(tname);
+      return dto;
+   }
+
+   public List<TourDTO> getSelectList(String addr) {
+      List<TourDTO> list = dao.getSelectList(addr);
+      return list;
+   }
+}
